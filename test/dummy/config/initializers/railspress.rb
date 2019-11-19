8e37db35609c794f838d40dea3e94257f25f60af
True Soft <truesoft@ymail.com>
2019-11-19 14:48:57 +0200

    Initializers

--- test/dummy/config/initializers/railspress.rb
+++ test/dummy/config/initializers/railspress.rb
@@ -2,7 +2,8 @@ Railspress.setup do |config|
   config.ABSPATH = "https://blog.truesoft.ro/"   # Wordpress URL
   config.WPINC =  "wp-includes"
   # config.UPLOADS = "images"
-  config.WP_CONTENT_DIR = "https://blog.truesoft.ro/wp-content" # ABSPATH + 'wp-content'
+  config.WP_CONTENT_DIR = "https://blog.truesoft.ro/wp-content"
+  config.WP_CONTENT_URL = "https://blog.truesoft.ro/wp-content"
   config.WP_POST_REVISIONS = true
 
   config.TS_READONLY_OPTIONS = %w(theme_mods_twentynineteen)
