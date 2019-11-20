feae9806baf003c356b9623a50fdb7d127655aa5
True Soft <truesoft@ymail.com>
2019-11-20 22:19:02 +0200

    View options

--- config/initializers/railspress.rb
+++ config/initializers/railspress.rb
@@ -5,7 +5,7 @@ module Railspress
 
   ::READONLY_OPTIONS = %w(siteurl home blogname blogdescription admin_email
     new_admin_email sidebars_widgets category_base tag_base page_for_posts page_on_front template stylesheet
-    gmt_offset permalink_structure widget_recent-posts WPLANG show_on_front)
+    date_format gmt_offset permalink_structure widget_recent-posts WPLANG show_on_front)
   ::EDITABLE_OPTIONS = []
 
 
