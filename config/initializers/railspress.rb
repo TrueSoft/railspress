4af40234095ef5a6332d37eff98a0bd774b3e8eb
True Soft <truesoft@ymail.com>
2019-11-21 14:50:10 +0200

    Cleanup

--- config/initializers/railspress.rb
+++ config/initializers/railspress.rb
@@ -1,13 +1,8 @@
-require 'railspress/taxonomy_lib'
-
 module Railspress
-  include Railspress::TaxonomyLib
 
   ::READONLY_OPTIONS = %w(siteurl home blogname blogdescription admin_email
     new_admin_email sidebars_widgets category_base tag_base page_for_posts page_on_front template stylesheet
     date_format gmt_offset permalink_structure widget_recent-posts WPLANG show_on_front)
   ::EDITABLE_OPTIONS = []
-
-
 
 end
