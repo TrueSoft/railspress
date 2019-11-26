64018ed39e17a79916516accce57ce5983ae04f0
True Soft <truesoft@ymail.com>
2019-11-26 16:02:07 +0200

    Add option in view table

--- config/initializers/railspress.rb
+++ config/initializers/railspress.rb
@@ -1,7 +1,7 @@
 module Railspress
 
-  ::READONLY_OPTIONS = %w(siteurl home blogname blogdescription admin_email
-    new_admin_email sidebars_widgets category_base tag_base page_for_posts page_on_front template stylesheet
+  ::READONLY_OPTIONS = %w(siteurl home blogname blogdescription admin_email new_admin_email
+    sidebars_widgets category_base tag_base page_for_posts page_on_front theme_mods_* template stylesheet
     date_format gmt_offset permalink_structure widget_recent-posts WPLANG show_on_front)
   ::EDITABLE_OPTIONS = []
 
