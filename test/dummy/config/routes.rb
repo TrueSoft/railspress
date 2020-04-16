ce687c44edccbac1f2ba76c49dd10591da0d5533
True Soft <truesoft@ymail.com>
2020-04-16 18:28:36 +0300

    updating tests for posts controller - refs #13

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'news/:year' => 'railspress/posts#by_year', constraints: {year: /20[12]\d/}, as: :news_of_year
   get 'news/:year/:month' => 'railspress/posts#by_month', constraints: {year: /20[12]\d/, month: /(0?\d)|10|11|12/}
   scope :news do
-    get ':slug' => 'railspress/posts#show'
-    get 'tag/:slug' => 'railspress/posts#tag'
-    get 'category/:slug' => 'railspress/posts#category'
+    get ':slug' => 'railspress/posts#single'
+    get 'tag/:slug' => 'railspress/posts#archive', defaults: {taxonomy: 'post_tag'}
+    get ':taxonomy/:slug' => 'railspress/posts#archive'
   end
 
   get 'page/*slug' => 'railspress/pages#show', as: :show_page
