abebc9c12e605d5ab72725717c93b89625b85e92
True Soft <truesoft@ymail.com>
2021-07-08 16:28:36 +0300

    Minor fix for posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Railspress::Engine.routes.draw do
   if !Railspress.posts_permalink_prefix.nil?
     scope path: Railspress.posts_permalink_prefix do
       get '' => 'railspress/home#posts', as: :all_posts
-      get 'page/:page' => 'railspress/home#posts'
-      get ':year' => 'railspress/archive#year_archive', constraints: {year: /20[12]\d/}, as: :year_archive_posts
+      get 'page/:page' => 'railspress/home#posts', defaults: {language: I18n.default_locale.to_s}
+      get ':year' => 'railspress/archive#year_archive', constraints: {year: /20[12]\d/}, defaults: {page: '1'}, as: :year_archive_posts
       get ':year/:monthnum' => 'railspress/archive#month_archive', constraints: {year: /20[12]\d/, monthnum: /(0?\d)|10|11|12/}
 
       get 'tag/:slug' => 'railspress/archive#taxonomy', defaults: {taxonomy: 'post_tag'}
